885ffd2bc67c828a60565bf450091035c98a6286
Tougashiro <muhammadadryan505@gmail.com>
2023-12-08 20:43:48 +0700

    Route & ViewController

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ViewController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::get('/', function(){
-    return view('layouts.main');
+Route::group(['middleware' => []], function () {
+   Route::controller(ViewController::class)->group(function(){
+    Route::get('/', 'main')->name('beranda');
+   });
 });
