78ba58a4141a109fa39f0d1bad132906b5ba78e2
Ridho A <ridhoawwaludin@gmail.com>
2023-12-08 21:23:42 +0700

    perbaiki loader

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -30,11 +30,9 @@
     <main>
         <!-- Hero -->
         <div class="preloader bg-dark flex-column justify-content-center align-items-center">
-            {{-- <div id="loader-logo"> --}}
+            <svg id="loader-logo" xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 64 78.4">
                 <img src="{{asset('assets/img/carousel/image-1.jpg')}}" alt="">
-            {{-- </div> --}}
-            {{-- <svg id="loader-logo" xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 64 78.4">
-                <path fill="#fff" d="M10,0h1.2V11.2H0V10A10,10,0,0,1,10,0Z"/>
+                {{-- <path fill="#fff" d="M10,0h1.2V11.2H0V10A10,10,0,0,1,10,0Z"/>
                 <rect fill="none" stroke="#fff" stroke-width="11.2" x="40" y="17.6" width="0" height="25.6"/>
                 <rect fill="none" stroke="#fff" stroke-opacity="0.4" stroke-width="11.2" x="23" y="35.2" width="0" height="25.6"/>
                 <path fill="#fff" d="M52.8,35.2H64V53.8a7,7,0,0,1-7,7H52.8V35.2Z"/>
@@ -45,8 +43,8 @@
                 <rect fill="none" stroke="#fff" stroke-width="11.2" x="40.2" y="49.6" width="0" height="11.2"/>
                 <path fill="#fff" d="M17.6,67.2H28.8v1.2a10,10,0,0,1-10,10H17.6V67.2Z"/>
                 <rect fill="none" stroke="#fff" stroke-opacity="0.4" stroke-width="28.8" x="31.6" width="0" height="11.2"/>
-                <rect fill="none" stroke="#fff" x="14" stroke-width="28.8" y="17.6" width="0" height="11.2"/>
-            </svg> --}}
+                <rect fill="none" stroke="#fff" x="14" stroke-width="28.8" y="17.6" width="0" height="11.2"/> --}}
+            </svg>
         </div>
         <section class="section-header overflow-hidden pt-7 pt-lg-8 pb-9 pb-lg-12 bg-primary text-white">
             <div class="container">
