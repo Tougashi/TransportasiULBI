8f17e1fba4201ea531d94a692bc0c076e33e696a
Ridho A <ridhoawwaludin@gmail.com>
2023-12-12 21:24:30 +0700

    Menambahkan container class
    Menambahkan container class di section Kegiatan mhs dan berita

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -101,9 +101,9 @@
                 </div>
             </div>
 
-            <div class="">
+            <div class="container-fluid px-5">
                 <h3 class="text-center fw-bolder mt-5 mb-4">Berita Terbaru</h3>
-                <div class="row mt-5 gap-3 d-flex justify-content-center">
+                <div class="row mt-5 gap-3 d-flex justify-content-center px-5">
                     <div class="col-12 col-lg-3 col-md-4 mb-lg-0 m-0">
                         <div class="card shadow-sm">
                             <img src="{{ asset('/assets/img/blog/image-1.jpg') }}" class="card-img-top rounded-top"
@@ -179,9 +179,9 @@
                 </div>
             </div>
 
-            <div class="">
+            <div class="container-fluid px-5 pt-5">
                 <h3 class="text-center fw-bolder mt-5 mb-4">Kegiatan Mahasiswa Prodi : S1 Manajemen Transportasi</h3>
-                <div class="row mt-5 gap-3 d-flex justify-content-center">
+                <div class="row mt-5 gap-3 d-flex justify-content-center px-5">
                     <div class="col-12 col-lg-3 col-md-4 mb-lg-0 m-0">
                         <div class="card shadow-sm">
                             <img src="{{ asset('/assets/img/blog/image-1.jpg') }}" class="card-img-top rounded-top"
