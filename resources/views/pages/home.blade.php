1daba976de9314b05e53a914c6781c88ab1b7c89
Tougashiro <muhammadadryan505@gmail.com>
2023-12-20 17:29:19 +0700

    asd

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -16,24 +16,7 @@
             </div>
         </section>
 
-        {{-- <section class="section-header overflow-hidden pt-7 pt-lg-8 pb-9 pb-lg-12 bg-primary text-white">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12 text-center">
-                        <div class="bootstrap-big-icon d-none d-lg-block">
-                        </div>
-                        <br><br>
-                        <h1 class="fw-bolder display-2">MANAJEMEN TRANSPORTASI</h1>
-                        <h2 class="lead fw-normal  mb-4 px-lg-10">UNIVERSITAS LOGISTIK & BISNIS INTERNASIONAL</h2>
-
-                    </div>
-                </div>
-            </div>
-            <figure class="position-absolute bottom-0 left-0 w-100 d-none d-md-block mb-n2"><svg class="fill-white"
-                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 3000 185.4">
-                    <path d="M3000,0v185.4H0V0c496.4,115.6,996.4,173.4,1500,173.4S2503.6,115.6,3000,0z"></path>
-                </svg></figure>
-        </section> --}}
+       
 
         <section class="section section-lg">
             <div class="container-fluid">
