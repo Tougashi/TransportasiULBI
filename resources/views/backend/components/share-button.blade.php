e6fec376478b78386e7fb502aca94dd274c0f608
Ridho A <ridhoawwaludin@gmail.com>
2024-01-06 22:18:32 +0700

    Update detail post

--- resources/views/backend/components/share-button.blade.php
+++ resources/views/backend/components/share-button.blade.php
@@ -1,29 +1,29 @@
 <div class="row d-flex justify-content-center">
-    <div class="col-2 me-lg-2 me-sm-3">
+    <div class="col-lg-2 col-md-3 col-3 me-lg-3 col-1">
        <a href="https://www.facebook.com/sharer.php?u={{ urlencode(url()->current()) }}">
             <div class="d-flex align-items-center mobile-text">
-                <i class="fa-brands fa-facebook me-2"></i> Share
+                <i class="fa-brands fa-facebook me-2"></i> <span class="d-lg-block d-none">Share</span>
             </div>
         </a>
     </div>
-    <div class="col-2 me-lg-2 me-sm-6">
+    <div class="col-lg-2 col-md-3 col-3 me-lg-3 col-1">
         <a href="https://twitter.com/share?url={{ url()->current() }}">
             <div class="d-flex align-items-center mobile-text">
-                <i class="fa-brands fa-twitter me-2"></i> Tweet
+                <i class="fa-brands fa-twitter me-2"></i> <span class="d-lg-block d-none">Tweet</span>
             </div>
         </a>
     </div>
-    <div class="col-2 me-lg-5 me-sm-5">
+    <div class="col-lg-2 col-md-3 col-3 me-lg-5 col-1">
         <a href="https://api.whatsapp.com/send?text={{ url()->current() }}">
             <div class="d-flex align-items-center mobile-text">
-                <i class="fa-brands fa-whatsapp me-2"></i> WhatsApp
+                <i class="fa-brands fa-whatsapp me-2"></i> <span class="d-lg-block d-none">WhatsApp</span>
             </div>
         </a>
     </div>
-    <div class="col-4" id="copyLinkWrapper">
+    <div class="col-lg-4 col-md-3 ms-lg-4 col-3" id="copyLinkWrapper">
         <a href="#">
             <div class="d-flex align-items-center mobile-text" id="copyLink">
-                <i class="fa-solid fa-share me-2"></i> Salin Tautan
+                <i class="fa-solid fa-share me-2"></i> <span class="d-lg-block d-none">Salin Tautan</span>
             </div>
         </a>
     </div>
@@ -39,6 +39,6 @@
         });
 
     </script>
-    
+
 @endpush
 
